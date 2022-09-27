bc11843621733c0f44a9a9d6a5b6d36fd5d0dda7
kench <kench.h.wong@gmail.com>
2022-09-27 22:21:49 +0800

    zx

--- cxx/Backend.cpp
+++ cxx/Backend.cpp
@@ -65,36 +65,6 @@ int Backend::connRemote()
 	//异步 conn 任务提交成功
 	return 0;
 }
-//
-//int Backend::addToSendChains(const std::shared_ptr<PACKET>& spPacket)
-//{
-//	if (_enConnStatus::connected != m_auStaus.load())
-//		return -1;
-//
-//	//TODO:stop
-//	//auto spThis = shared_from_this();
-//	m_spStrand->post([this, spPacket]()
-//	{
-//		m_Queue.push(std::move(spPacket));
-//
-//		//触发发送
-//		if (1 == m_Queue.size())
-//			triggerSend(spPacket);
-//	});
-//
-//	return 0;
-//}
-
-//int Backend::toldShutdown()
-//{
-//	return 0;
-//}
-//
-//_enConnStatus Backend::getStatus()
-//{
-//	auto s = m_auStaus.load();
-//	return s;
-//}
 
 void Backend::onConned(std::shared_ptr<socket>spSocket, const boost::system::error_code ec)
 {
@@ -111,71 +81,3 @@ void Backend::onConned(std::shared_ptr<socket>spSocket, const boost::system::err
 
 	doRecv();
 }
-//
-//void Backend::triggerSend(const std::shared_ptr<PACKET>& spPacket)
-//{
-//	m_spSocket->async_send(boost::asio::buffer(spPacket->spszBuff.get(), spPacket->uRealLen),
-//		std::bind(&Backend::onSend, this, std::placeholders::_1)
-//	);
-//}
-//
-//void Backend::onSend(const boost::system::error_code& ec)
-//{
-//	if (ec) {
-//		//通知失败,断开连接,清空队列
-//		return;
-//	}
-//
-//	m_spStrand->post([this](){
-//		if (!m_Queue.empty())
-//			m_Queue.pop();
-//
-//		//Read_Lock rStopStatusLock(m_rwmutStoped);
-//		//if (m_bStoped)
-//		//	return;
-//
-//		if (!m_Queue.empty()){
-//			auto spNextSend = m_Queue.front();
-//			triggerSend(spNextSend);
-//		}
-//	});
-//}
-
-
-//void Backend::doRecv()
-//{
-//	constexpr unsigned int uBufferSize = 4096;
-//	boost::shared_array<char>spszBuffer(new char[uBufferSize]);
-//	memset(spszBuffer.get(), 0, uBufferSize);
-//
-//	//m_abWorking = true;
-//
-//	m_spSocket->async_read_some(boost::asio::buffer(spszBuffer.get(), uBufferSize),
-//		boost::asio::bind_executor(*m_spStrand,
-//		std::bind(&Backend::onRecv, this,
-//			std::placeholders::_1,
-//			std::placeholders::_2,
-//			spszBuffer
-//		))
-//	);
-//}
-
-//void Backend::onRecv(\
-//	const boost::system::error_code& ec,
-//	size_t  uRecvSize,
-//	boost::shared_array<char>spszBuff
-//)
-//{
-//	//TODO
-//	//if (m_fnOnDataIncomm) {
-//	//	m_fnOnDataIncomm(std::move(spszBuff), uRecvSize, ec.value(), ec.message().c_str());
-//	//}
-//
-//	//TODO: 连接异常处理
-//	if (ec) {
-//		//m_abWorking = false;
-//		return;
-//	}
-//
-//	doRecv();
-//}
